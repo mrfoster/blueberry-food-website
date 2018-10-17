8af20cec713ecb76da24ca3965e6fac882d2d25e
Chris Foster <chris.foster@techdept.co.uk>
2018-10-18 00:55:22 +0100

    Fixed contact details

--- src/components/contact.js
+++ src/components/contact.js
@@ -25,8 +25,8 @@ const Contact = ({ data }) => {
         ))}
       </address>
       <em>Tel:</em>
-      <a href={`tel:${data.schema.telephone}`}>{data.telephone}</a> <br />
-      <em>Email:</em> <a href={`mailto:${data.schema.email}`}>{data.email}</a>
+      <a href={`tel:${data.schema.telephone}`}>{data.schema.telephone}</a> <br />
+      <em>Email:</em> <a href={`mailto:${data.schema.email}`}>{data.schema.email}</a>
       <br />
       {data.page.vcf && <a href={data.page.vcf.publicURL}>Add to contacts</a>}
     </section>
