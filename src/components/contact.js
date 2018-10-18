5c192553bcfb450ea15ec2ab54889805cc7a209a
Chris Foster <chris.foster@techdept.co.uk>
2018-10-19 00:38:12 +0100

    Readded test content

--- src/components/contact.js
+++ src/components/contact.js
@@ -27,15 +27,13 @@ const Contact = ({ data }) => {
       {data.schema.telephone && (
         <>
           <em>Tel:</em>
-          <a href={`tel:${data.schema.telephone}`}>
-            {data.schema.telephone}
-          </a>{' '}
+          <a href={`tel:${data.schema.telephone}`}>{data.schema.telephone}</a>
           <br />
         </>
       )}
       {data.schema.email && (
         <>
-          <em>Email:</em>{' '}
+          <em>Email:</em>
           <a href={`mailto:${data.schema.email}`}>{data.schema.email}</a>
           <br />
         </>
